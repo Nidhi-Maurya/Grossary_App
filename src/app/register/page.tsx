0deb1a5d59c0cb256da4a98a500dd184dd5e8177
Nidhi Maurya <mauryanidhi1610@gmail.com>
2026-07-15 01:16:06 +0530

    implement register form page ui design

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,24 +1,18 @@
-
-"use client"
+"use client";
 import { useState } from "react";
 
-import Welcome from "@/components/Welcome"
+import Welcome from "@/components/Welcome";
 import RegisterForm from "@/components/RegisterForm";
-  export default function Register(){
+export default function Register() {
+  const [step, setStep] = useState(1);
 
-    const [step,setStep] = useState(1)
-
-
-
-
-
-
-
-    return (
-      <>
-
-{step===1  ? <Welcome nextStep={setStep} /> : <RegisterForm previousStep={setStep} />}
-      
-      </>
-    )
-  }
+  return (
+    <>
+      {step === 1 ? (
+        <Welcome nextStep={setStep} />
+      ) : (
+        <RegisterForm previousStep={setStep} />
+      )}
+    </>
+  );
+}
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,40 +1,142 @@
-import {ArrowLeft} from "lucide-react";
+import {
+  ArrowLeft,
+  EyeIcon,
+  EyeOff,
+  Leaf,
+  Lock,
+  LogIn,
+  Mail,
+  User,
+} from "lucide-react";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import Image from "next/image";
+import google from "@/assets/google.avif";
 
-type propType={
-  previousStep:(n:number)=> void
-}
+type propType = {
+  previousStep: (n: number) => void;
+};
 
+export default function RegisterForm({ previousStep }: propType) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-
-export default function RegisterForm({previousStep}:propType){
   return (
-   <div className= "flex flex-col items-center justify-center min-h-screen px-6 py-10 bg-white relative">
+    <div className="flex flex-col items-center justify-center min-h-screen px-6 py-10 bg-white relative">
+      <div
+        className="absolute top-6 left-6 flex items-center gap-2 text-green-700 hover:text-green-800 transitions-colors cursor-pointer"
+        onClick={() => previousStep(1)}
+      >
+        <ArrowLeft className="w-5 h-5 " />
 
-            <div className="absolute top-6 left-6 flex items-center gap-2 text-green-700 hover:text-green-800 transitions-colors cursor-pointer" 
-            onClick={()=>previousStep(1)}
-            
-            
-            >
+        <span className="font-medium"> Back </span>
+      </div>
 
-               <ArrowLeft className="w-5 h-5 " />
+      <motion.h1
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        className="text-3xl md:text-4xl font-extrabold text-green-700 mb-2"
+      >
+        Create Account
+      </motion.h1>
 
-   <span className = "font-medium" >  Back   </span>
- 
-               </div>
+      <p className="text-green mb-8 flex items-center  text-medium">
+        {" "}
+        Join Snapcart today <Leaf className="w-5 h-5 text-green-700" />{" "}
+      </p>
 
+      <motion.form
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
+        className="flex flex-col gap-5 w-full max-w-sm"
+      >
+        <div className="relative">
+          <User className="absolute left-3 top-3.5 w-5 h-5 text-gray-400" />
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-2 focus:ring-green-500 focus:outline-none "
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
 
-          <motion.h1 className="text-3xl md:text-4xl font-extrabold text-green-700 mb-3">
-          Create Account 
-          </motion.h1>
+        <div className="relative">
+          <Mail className="absolute left-3 top-3.5 w-5 h-5 text-gray-400" />
+          <input
+            type="text"
+            placeholder="Your Email"
+            className="w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-2 focus:ring-green-500 focus:outline-none "
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
 
+        <div className="relative">
+          <Lock className="absolute left-3 top-3.5 w-5 h-5 text-gray-400" />
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Your Password"
+            className="w-full border border-gray-300 rounded-xl py-3 pl-10 pr-4 text-gray-800 focus:ring-2 focus:ring-green-500 focus:outline-none "
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
 
+          {showPassword ? (
+            <EyeOff
+              className="absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer "
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <EyeIcon
+              className="absolute right-3 top-3.5 w-5 h-5 text-gray-500 cursor-pointer "
+              onClick={() => setShowPassword(true)}
+            />
+          )}
+        </div>
 
+        <button>
+          {(() => {
+            const FormValidation =
+              name !== "" && email !== "" && password !== "";
+            return (
+              <button
+                disabled={!FormValidation}
+                className={`w-full font-semibold py-3 rounded-xl transition-all duration-200 shadow-md inline-flex items-center justify-center gap-2 cursor-pointer ${
+                  FormValidation
+                    ? "bg-green-600 hover:bg-green-700 text-white"
+                    : "bg-gray-300 text-gray-500 cursor-not-allowed"
+                }`}
+              >
+                Register
+              </button>
+            );
+          })()}
+        </button>
 
+        <div className="flex items-center gap-2 text-gray-400 text-sm mt-2 font-semibold">
+          <span className="flex-1 h-px bg-gray-200"></span>
+          OR
+          <span className="flex-1 h-px bg-gray-200"></span>
+        </div>
 
+        <button className="w-full flex  items-center justify-center gap-3 border border-gray-300 hover:bg-gray-50 py-3 rounded-xl text-gray-700 font-medium transition-all duration-200">
+          <Image src={google} alt="google logo" width={40} height={40} />
+          Continue with Google
+        </button>
+      </motion.form>
 
-
+      <p className="text-gray-600 mt-6 text-sm flex items-center gap-1 cursor-pointer ">
+        {" "}
+        Already have an account ? <LogIn className="w-4 h-4"/> 
+        <span className="text-green-600">
+          Sign in
+        </span>
+      </p>
     </div>
-
-  )
+  );
 }
