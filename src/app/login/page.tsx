aff29e76eedf1756543df5b3b77112edb2f05fb9
Nidhi Maurya <mauryanidhi1610@gmail.com>
2026-07-17 00:06:03 +0530

    login with google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -143,7 +143,7 @@ const handleLogin = async (e:React.FormEvent)=>{
           <span className="flex-1 h-px bg-gray-200"></span>
         </div>
 
-        <button className="w-full flex  items-center justify-center gap-3 border border-gray-300 hover:bg-gray-50 py-3 rounded-xl text-gray-700 font-medium transition-all duration-200">
+        <button onClick={()=>signIn("google")} className="w-full flex  items-center justify-center gap-3 border border-gray-300 hover:bg-gray-50 py-3 rounded-xl text-gray-700 font-medium transition-all duration-200">
           <Image src={google} alt="google logo" width={40} height={40} />
           Continue with Google
         </button>
