51fc30e0a9b9f5ccf86ea5073c52648516742978
Nidhi Maurya <mauryanidhi1610@gmail.com>
2026-07-21 22:51:19 +0530

    add provide for login

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,11 +1,7 @@
  
  "use client";
 
-import { SessionProvider } from "next-auth/react";
-
- 
- 
- 
+import { SessionProvider } from "next-auth/react"; 
  export default function Provider({children}:{children:React.ReactNode}){
 
   return (
